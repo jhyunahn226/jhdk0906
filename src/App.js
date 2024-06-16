29263e1c5481892c6e37779ad0367c5b1c68d74f
JHyun Ahn <jhyunahn226@gmail.com>
2024-06-16 14:53:33 +0900

    Footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Page1 from "components/Page1";
 import Page2 from "components/Page2";
 import Page3 from "components/Page3";
 import Page4 from "components/Page4";
+import Footer from "components/footer";
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
@@ -136,6 +137,7 @@ const App = () => {
           handleImageClick(image, index, photos)
         }
       />
+      <Footer />
       <ImageModal
         open={modalOpen}
         onClose={handleCloseModal}
